16dae21ddef2a521c3110f83052752a37f636eb0
g-daipai <g-daipai@example.local>
2026-06-15 22:02:12 +0800

    乱码bug

--- yahoo-plugin/encoding.test.js
+++ yahoo-plugin/encoding.test.js
@@ -7,6 +7,7 @@ const badPatterns = [
   { name: 'UTF-8 BOM', pattern: /^\uFEFF/ },
   { name: 'replacement character', pattern: /\uFFFD/ },
   { name: 'gbk replacement mojibake', pattern: /\u95bf\u7194\u67bb\u93b7/ },
+  { name: 'common UTF-8 decoded as GBK mojibake', pattern: /[\u95c1\u95c2\u95bb\u6fe0\u7f02\u5a75\u9227\u951f\u7e1e\u9518]/ },
   { name: 'BOM mojibake', pattern: /\u9518|\u7e1e/ },
   { name: 'copied truncation marker', pattern: /<[^>\r\n]*(?:\u672a\u663e\u793a|\u672a\u986f\u793a|not shown)[^>\r\n]*>/i }
 ];
